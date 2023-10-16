dec429b5ff304023d1342782fc96d007e603d548
kenneth E. Gbeti <95012008+kennethEdwin147@users.noreply.github.com>
2023-10-15 23:09:36 -0400

    Update and rename homebrew to homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -4,3 +4,10 @@
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+
+# https://cnu.hashnode.dev/set-up-homebrew-on-linux-with-fish-shell
+# Add Homebrew to your PATH
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+echo 'eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.config/fish/config.fish
+
