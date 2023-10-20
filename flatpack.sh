a61d381c725fe3b1552767d1478cdcf9d571bb5e
kenneth E. Gbeti <95012008+kennethEdwin147@users.noreply.github.com>
2023-10-20 11:55:15 -0400

    Create flatpack.sh

---
+++ flatpack.sh
@@ -0,0 +1,5 @@
+# https://flatpak.org/setup/Chrome%20OS
+sudo apt install flatpak
+# Add the Flathub repository
+# Flathub is the best place to get Flatpak apps. To enable it, run:
+flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
